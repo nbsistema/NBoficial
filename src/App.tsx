42773110f926c7b2ae33c799c420f5d5d9ef0087
nbsistema <nbconsultoriasistema@gmail.com>
2025-08-27 11:11:39 -0300

    Added vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './app/page'
+import LoginPage from './app/login/page'
+import AdminDashboard from './app/admin/page'
+import CTRDashboard from './app/ctr/page'
+import ParceiroDashboard from './app/parceiro/page'
+import CheckupDashboard from './app/checkup/page'
+import UnauthorizedPage from './app/unauthorized/page'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/ctr" element={<CTRDashboard />} />
+        <Route path="/parceiro" element={<ParceiroDashboard />} />
+        <Route path="/checkup" element={<CheckupDashboard />} />
+        <Route path="/unauthorized" element={<UnauthorizedPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
